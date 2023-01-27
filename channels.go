7b593ea780e544dba5b10078b0c27a943a81c555
arieltraver <arieltraver@users.noreply.github.com>
2023-01-27 13:32:16 -0500

    channels with buffer

--- channels.go
+++ channels.go
@@ -7,6 +7,11 @@ Channels
 --> pipes to send data to another thread / routine.
 --> sending end automatically blocks until receiving end is ready
 --> helps goroutines synchronize data without explicit locks
+--> buffered channels
+		--> maximum size of a channel
+		--> only sends when the buffer is full
+		--> receive requests block when the buffer is empty.
+		--> overfill: throws an error!
 */
 
 func sum(s []int, c chan int) {
@@ -24,6 +29,13 @@ func main() {
 	go sum(s[:len(s)/2], c) //owns the channel first
 	go sum(s[len(s)/2:], c) //gets the channel once the first routine finishes
 	x, y := <-c, <-c        // receive from c
-
 	fmt.Println(x, y, x+y)
+
+	//using a buffer
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	//ch <- 3 overfilling the buffer causes an error
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }

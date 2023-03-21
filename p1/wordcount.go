e7896b1685fcf1e93f0e7c52a1907a3cd812f818
arieltraver <arieltraver@users.noreply.github.com>
2023-03-21 14:49:58 -0400

    fixed bug

--- p1/wordcount.go
+++ p1/wordcount.go
@@ -24,11 +24,13 @@ import (
 	"time"
 	"math"
 )
-var MAXWORDSIZE int = 30
-var READSIZE int = 10000
+var MAXWORDSIZE int64 = 30
+var READSIZE int64 = 100000
 var NONLETTER = regexp.MustCompile(`[^a-zA-Z0-9]+`)
 var M_OUTPUT = "/multi.txt"
 var S_OUTPUT = "/single.txt"
+var NUMTESTS = 1
+var INPUT = "justbig"
 
 func readFiles(directory string)([]string, int) {
 	filepaths, err := os.ReadDir(directory)
@@ -66,17 +68,23 @@ type SafeMap struct {
 
 
 
-func grabSomeText(file *os.File, readSize int) ([]byte, error) {
+func grabSomeText(file *os.File) ([]byte, error) {
+	fInfo, _ := file.Stat()
+	readSize := READSIZE
+	if readSize > fInfo.Size() {
+		readSize = fInfo.Size()
+	}
 	buff := make([]byte, readSize, readSize + MAXWORDSIZE) //avoid reallocation
 	_, err := file.Read(buff) // read the length of buffer from file
 	if err != nil {
 		if err == io.EOF {
-			return nil, io.EOF //reached end of file
+			return buff, io.EOF //reached end of file
 		} else {
 			log.Fatal(err)
 		}
 	}
-	extra, err2 := getStr(file) // read till next space if present
+	//fmt.Println("bytesRead", bytesRead)
+	extra, err2 := getRest(file) // read till next space if present
 	buff = append(buff, extra...)
 	if err2 == io.EOF {
 		return buff, io.EOF
@@ -85,21 +93,19 @@ func grabSomeText(file *os.File, readSize int) ([]byte, error) {
 }
 // Reads from file up to encountering whitespace. Used to account for words
 // that may be cut off when reading by amount of bytes. Returns string
-func getStr(file *os.File) (string, error) {
-	extra := strings.Builder{}
-	b := make([]byte, 1)
+func getRest(file *os.File) ([]byte, error) {
+	b := make([]byte, 0, MAXWORDSIZE)
 	for {
-		_, err := file.Read(b)
+		temp := make([]byte, 1)
+		bRead, err := file.Read(temp)
 		if err != nil && err != io.EOF {
 			log.Fatal(err)
 		} else if err == io.EOF {
-			return extra.String(), io.EOF
-		} else if b[0] == ' ' {
-			return extra.String(), nil
-		} else {
-			extra.Grow(1)
-			extra.WriteByte(b[0])
+			return b, io.EOF
+		} else if bRead == 0 || NONLETTER.Match(temp){
+			return b, nil
 		}
+		b = append(b, temp[0])
 	}
 }
 
@@ -113,14 +119,23 @@ func multiThreaded(files []string) {
 			file.Close()
 			log.Fatal(err)
 		}
-		txt, err2 := grabSomeText(file, READSIZE) //read from this file
-		for text, err3 := txt, err2; err3 == nil; text, err3 = grabSomeText(file, READSIZE){
+		txt, err2 := grabSomeText(file) //read from this file
+		if err2 == io.EOF {
 			waitgroup.Add(1)
-			go wordRoutine(text, &globalMap, &waitgroup) //routine for this chunk.
-			/*I am curious about the "lifespan" of this byte array (text).
-			//Does go compiler's garbage collector free the memory after the routine completes?
-			//It could technically still be used in this function
-			but since it's not, I am wondering if garbage collector takes care of it*/
+			go wordRoutine(txt, &globalMap, &waitgroup)	
+		} else {
+			for {
+				waitgroup.Add(1)
+				go wordRoutine(txt, &globalMap, &waitgroup) //routine for this chunk.
+				/*I am curious about the "lifespan" of this byte array (text).
+				Does go compiler's garbage collector free the memory after the routine completes?
+				It could technically still be used in this function
+				But since it's not, I am wondering if garbage collector takes care of it*/
+				if err2 == io.EOF {
+					break;
+				}
+				txt, err2 = grabSomeText(file)
+			}
 		}
 		file.Close()
 	}
@@ -159,7 +174,6 @@ func wordRoutine(text []byte, globalMap *SafeMap, waitgroup *sync.WaitGroup) {
 
 func countSomeFiles(files []string) *map[string]int {
 	counts := make(map[string] int) //store word counts by key which is the word itself
-	var nonLetter = regexp.MustCompile(`[^a-zA-Z0-9]+`)
 	for _, filename := range(files) {
 		file, err := os.Open(filename) //file pointer
 		if err != nil {
@@ -173,10 +187,10 @@ func countSomeFiles(files []string) *map[string]int {
 		for scanner.Scan() { //reads until EOF OR until the limit
 			word := scanner.Text()
 			word = strings.ToLower(word) //lowercase-ify
-			word = nonLetter.ReplaceAllString(word, " ") //get rid of extra characters
+			word = NONLETTER.ReplaceAllString(word, " ") //get rid of extra characters
 			words := strings.Split(word, " ") //split words by char
 			for _, wd := range(words) {
-				wd2 := nonLetter.ReplaceAllString(wd, "") //get rid of spaces
+				wd2 := NONLETTER.ReplaceAllString(wd, "") //get rid of spaces
 				counts[wd2] = counts[wd2] + 1 //increment word count in the dictionary
 			}
 		}
@@ -212,17 +226,16 @@ func writeMapToFile(filename string, counts *map[string]int) error {
 func countRoutine(file *os.File, c chan *map[string]int, waitgroup *sync.WaitGroup) {
 	defer waitgroup.Done()
 	//fmt.Println("thread is going!")
-	var nonLetter = regexp.MustCompile(`[^a-zA-Z0-9]+`)
 	scanner := bufio.NewScanner(file) //buffered i/o: creates a pipe for reading
 	counts := make(map[string]int) //store word counts by key which is the word itself
 		scanner.Split(bufio.ScanWords) //break reading pattern into words
 		for scanner.Scan() { //reads until EOF
 			word := scanner.Text() //get a word
 			word = strings.ToLower(word) //lowercase-ify the word
-			word = nonLetter.ReplaceAllString(word, " ") //replace extra characters with space
+			word = NONLETTER.ReplaceAllString(word, " ") //replace extra characters with space
 			words := strings.Split(word, " ") //split words by s
 			for _, wd := range(words) {
-				wd2 := nonLetter.ReplaceAllString(wd, "") //get rid of spaces
+				wd2 := NONLETTER.ReplaceAllString(wd, "") //get rid of spaces
 				counts[wd2] = counts[wd2] + 1 //increment word count in the dictionary
 			}
 		}
@@ -273,15 +286,14 @@ func main() {
         	log.Fatal(err)
     	}
 	}
-	files, error := readFiles("./input")
+	files, error := readFiles(INPUT)
 	switch error {
 	case 1:
 		log.Fatal("Error reading file names from directory") //calls to panic
 	case 2:
 		log.Fatal("Please store only text files in your directory")
 	default:
-		runTests(files)
-
+		runTests(files, NUMTESTS)
 	}
 	
 	// TODO: add argument processing and run both single-threaded and multi-threaded functions
@@ -314,11 +326,11 @@ func testFunc(foo func([]string), input []string, iterations int) (float64, floa
 }
 
 func runTests(files []string, numTests int) {
-	avgSingle, stdevSingle := testFunc(singleThreaded, files, numTests)
+	//avgSingle, stdevSingle := testFunc(singleThreaded, files, numTests)
 	avg0, stdev0 := testFunc(multiThreaded0, files, numTests)
 	avg, stdev := testFunc(multiThreaded, files, numTests)
 
-	fmt.Println("single threaded:", avgSingle, stdevSingle)
+	//fmt.Println("single threaded:", avgSingle, stdevSingle)
 	fmt.Println("multi without split:", avg0, stdev0)
 	fmt.Println("multi with split:", avg, stdev)
 }
